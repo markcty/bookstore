530c89cd790f0ad5b640dcb2a759076f5fbc9a8e
markcty <markcty@outlook.com>
2021-03-27 16:32:10 +0800

    fix checkout display issue

--- bookstore-frontend/src/pages/Checkout/index.js
+++ bookstore-frontend/src/pages/Checkout/index.js
@@ -88,87 +88,92 @@ export default class Checkout extends React.Component {
         return (
             <Content className={"page"}>
                 <Row justify={"center"}>
-                    <Col sm={24} md={16} lg={16} xl={14}>
-                        <h1 style={{
-                            borderBottom: "2px solid #E1E8EE",
-                            paddingLeft: 12,
-                            paddingBottom: 12,
-                            textAlign: "left",
-                            fontSize: "1.8em"
-                        }}>
-                            Items
-                        </h1>
-                    </Col>
-                    <Col sm={24} md={16} lg={16} xl={14} style={{paddingBottom: 24}}>
-                        <Row gutter={48}>
-                            {items.map(book => {
-                                return (
-                                    <Col sm={24} md={12}>
-                                        <CartCard {...book}/>
-                                    </Col>
-                                )
-                            })}
+                    <Col xs={24} sm={20} md={18} lg={16} xl={14}>
+                        <Row justify={"center"}>
+                            <Col span={24}>
+                                <h1 style={{
+                                    borderBottom: "2px solid #E1E8EE",
+                                    paddingLeft: 12,
+                                    paddingBottom: 12,
+                                    textAlign: "left",
+                                    fontSize: "1.8em"
+                                }}>
+                                    Items
+                                </h1>
+                            </Col>
+                            <Col span={24} style={{paddingBottom: 24}}>
+                                <Row gutter={48}>
+                                    {items.map(book => {
+                                        return (
+                                            <Col span={24}>
+                                                <CartCard {...book}/>
+                                            </Col>
+                                        )
+                                    })}
+                                </Row>
+                            </Col>
+                            <Col span={24}>
+                                <h1 style={{
+                                    borderBottom: "2px solid #E1E8EE",
+                                    paddingLeft: 12,
+                                    paddingBottom: 12,
+                                    textAlign: "left",
+                                    fontSize: "1.8em"
+                                }}>
+                                    Shipping Information
+                                </h1>
+                            </Col>
+                            <Col span={24}
+                                 style={{display: "flex", justifyContent: "flex-start"}}>
+                                <Form {...layout} name="nest-messages" onFinish={onFinish}
+                                      validateMessages={validateMessages} style={{width: "100%"}}
+                                      ref={this.form}
+                                >
+                                    <Form.Item
+                                        name={['user', 'name']}
+                                        label="Name"
+                                        rules={[
+                                            {
+                                                required: true,
+                                            },
+                                        ]}
+                                    >
+                                        <Input/>
+                                    </Form.Item>
+                                    <Form.Item
+                                        name={['user', 'phone']}
+                                        label="Phone Number"
+                                        rules={[
+                                            {
+                                                required: true
+                                            },
+                                        ]}
+                                    >
+                                        <Input/>
+                                    </Form.Item>
+                                    <Form.Item name={['user', 'address']}
+                                               label="Address"
+                                               rules={[
+                                                   {
+                                                       required: true
+                                                   },
+                                               ]}
+                                    >
+                                        <Input/>
+                                    </Form.Item>
+                                    <Form.Item name={['user', 'Notes']} label="Notes">
+                                        <Input.TextArea/>
+                                    </Form.Item>
+                                </Form>
+                            </Col>
+                            <Col span={24}
+                                 style={{display: "flex", justifyContent: "flex-end", marginTop: 16}}
+                            >
+                                <h2 className={"payButton"} onClick={this.pay}>
+                                    Pay
+                                </h2>
+                            </Col>
                         </Row>
-                    </Col>
-                    <Col sm={24} md={16} lg={16} xl={14}>
-                        <h1 style={{
-                            borderBottom: "2px solid #E1E8EE",
-                            paddingLeft: 12,
-                            paddingBottom: 12,
-                            textAlign: "left",
-                            fontSize: "1.8em"
-                        }}>
-                            Shipping Information
-                        </h1>
-                    </Col>
-                    <Col sm={24} md={16} lg={16} xl={14} style={{display: "flex", justifyContent: "flex-start"}}>
-                        <Form {...layout} name="nest-messages" onFinish={onFinish}
-                              validateMessages={validateMessages} style={{width: "100%"}}
-                              ref={this.form}
-                        >
-                            <Form.Item
-                                name={['user', 'name']}
-                                label="Name"
-                                rules={[
-                                    {
-                                        required: true,
-                                    },
-                                ]}
-                            >
-                                <Input/>
-                            </Form.Item>
-                            <Form.Item
-                                name={['user', 'phone']}
-                                label="Phone Number"
-                                rules={[
-                                    {
-                                        required: true
-                                    },
-                                ]}
-                            >
-                                <Input/>
-                            </Form.Item>
-                            <Form.Item name={['user', 'address']}
-                                       label="Address"
-                                       rules={[
-                                           {
-                                               required: true
-                                           },
-                                       ]}
-                            >
-                                <Input/>
-                            </Form.Item>
-                            <Form.Item name={['user', 'Notes']} label="Notes">
-                                <Input.TextArea/>
-                            </Form.Item>
-                        </Form>
-                    </Col>
-                    <Col sm={24} md={16} lg={16} xl={14}
-                         style={{display: "flex", justifyContent: "flex-end", marginTop: 16}}
-                    >
-                        <h2 className={"payButton"} onClick={this.pay}>
-                            Pay
-                        </h2>
                     </Col>
                 </Row>
             </Content>
