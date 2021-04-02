d96eb6cf536bf0b544e21927352ad804d7078689
markcty <markcty@outlook.com>
2021-04-02 19:28:30 +0800

    rewrite login page

--- bookstore-frontend/src/pages/Login/index.js
+++ bookstore-frontend/src/pages/Login/index.js
@@ -1,84 +1,84 @@
 import React from "react";
 import "./index.css";
 import {Content} from "antd/es/layout/layout";
-import {Col, Form, Input, Row} from 'antd';
+import {Button, Form, Input} from 'antd';
 import {LockOutlined, UserOutlined} from '@ant-design/icons';
+import Checkbox from "antd/es/checkbox/Checkbox";
 
-export default class Login extends React.Component {
-    constructor(props) {
-        super(props);
-        this.form = React.createRef();
-        this.login = this.login.bind(this);
-    }
+export default function Login() {
+    const onFinish = (values) => {
+        console.log('Received values of form: ', values);
+    };
 
-    login() {
-        console.log("hello");
-        this.form.current.submit();
-    }
-
-    render() {
-        const onFinish = (values) => {
-            console.log('Received values of form: ', values);
-        };
-        return (
-            <Content className={"page"}>
-                <div id="card">
-                    <Row className={"cardContent"}>
-                        <Col span={24} className={"cardTitle"}
-                             style={{textAlign: "center", marginBottom: "32px", marginTop: "16px"}}>
-                            <h2 style={{paddingBottom: "8px"}}>LOGIN</h2>
-                            <div id={"underline-title"}/>
-                        </Col>
-                        <Col span={24} style={{display: "flex", justifyContent: "center"}}>
-                            <Form
-                                name="normal_login"
-                                className="login-form"
-                                initialValues={{
-                                    remember: true,
-                                }}
-                                onFinish={onFinish}
-                                style={{width: "80%"}}
-                                ref={this.form}
-                            >
-                                <Form.Item
-                                    name="username"
-                                    rules={[
-                                        {
-                                            required: true,
-                                            message: 'Please input your Username!',
-                                        },
-                                    ]}
-                                >
-                                    <Input prefix={<UserOutlined className="site-form-item-icon"/>}
-                                           placeholder="Username"/>
-                                </Form.Item>
-                                <Form.Item
-                                    name="password"
-                                    rules={[
-                                        {
-                                            required: true,
-                                            message: 'Please input your Password!',
-                                        },
-                                    ]}
-                                >
-                                    <Input
-                                        prefix={<LockOutlined className="site-form-item-icon"/>}
-                                        type="password"
-                                        placeholder="Password"
-                                    />
-                                </Form.Item>
-                            </Form>
-                        </Col>
-                        <Col span={24}
-                             style={{display: "flex", justifyContent: "center", marginTop: 32}}
-                        >
-                            <h2 className={"loginButton"} onClick={this.login}>
-                                Login
-                            </h2>
-                        </Col>
-                    </Row>
+    return (
+        <Content className={"page loginPage"} style={{
+            paddingTop: 48,
+            paddingBottom: 50,
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+        }}>
+            <div className={"loginCard"}
+                 style={{display: "flex", flexDirection: "column", paddingLeft: 48, paddingRight: 48}}>
+                <div className={"cardTitle"}
+                     style={{textAlign: "center", marginBottom: "32px", marginTop: "16px"}}>
+                    <h2 style={{paddingBottom: "8px"}}>LOGIN</h2>
+                    <div id={"underline-title"}/>
                 </div>
-            </Content>
-        )
-    }
+                <Form
+                    name="normal_login"
+                    className="login-form"
+                    initialValues={{
+                        remember: true,
+                    }}
+                    onFinish={onFinish}
+                >
+                    <Form.Item
+                        name="username"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input your Username!',
+                            },
+                        ]}
+                    >
+                        <Input prefix={<UserOutlined className="site-form-item-icon"/>}
+                               placeholder="Username"/>
+                    </Form.Item>
+                    <Form.Item
+                        name="password"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input your Password!',
+                            },
+                        ]}
+                    >
+                        <Input
+                            prefix={<LockOutlined className="site-form-item-icon"/>}
+                            type="password"
+                            placeholder="Password"
+                        />
+                    </Form.Item>
+                    <Form.Item>
+                        <Form.Item name="remember" valuePropName="checked" noStyle>
+                            <Checkbox>Remember me</Checkbox>
+                        </Form.Item>
+
+                        <a className="login-form-forgot" href="">
+                            Forgot password
+                        </a>
+                    </Form.Item>
+
+                    <Form.Item>
+                        <Button type="primary" htmlType="submit" className="login-form-button">
+                            Log in
+                        </Button>
+                        Or <a href="">register now!</a>
+                    </Form.Item>
+                </Form>
+            </div>
+        </Content>
+    )
+
 }
