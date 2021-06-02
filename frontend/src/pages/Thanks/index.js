09628d31f3a8bfb7b4a485ded27acbac267e619a
markcty <markcty@outlook.com>
2021-06-02 11:09:25 +0800

    add goto my orders

--- frontend/src/pages/Thanks/index.js
+++ frontend/src/pages/Thanks/index.js
@@ -1,5 +1,6 @@
 import { Layout } from "antd";
 import React from "react";
+import { Link } from "react-router-dom";
 import "./index.css";
 
 const { Content } = Layout;
@@ -8,7 +9,19 @@ export default function Thanks() {
   return (
     <Content className={"page"}>
       <div style={{ display: "flex", justifyContent: "center" }}>
-        <h1>Thank you for buying. Good Day!</h1>
+        <div>
+          <h1>Thank you for buying. Good Day!</h1>
+          <Link
+            to="/orders"
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              fontSize: "2em",
+            }}
+          >
+            Goto My Orders
+          </Link>
+        </div>
       </div>
     </Content>
   );
